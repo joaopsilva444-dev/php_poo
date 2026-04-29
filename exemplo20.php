b411910f6a91f0282c45364a670c75285118c698
João Pedro Miguel <joao.p.silva444@aluno.senai.br>
2026-04-29 16:40:13 -0300

    exemplo20 PHP

--- exemplo20.php
+++ exemplo20.php
@@ -19,3 +19,34 @@ class NotificadorSMS implements INotificador{
         echo"SMS enviado para {$destinatario}. Mensagem: {$mensagem}.";
     }
 }
+
+// Implementar: Whatsapp
+class Notificadorwhatsapp implements INotificador{
+    public function enviar($destinatario, $mensagem)
+    {
+        echo "Whatsapp enviado para {$destinatario}. Mensagem: {$mensagem}.";
+    }
+}
+
+// Classe que usa a interface
+class SistemaDeNotificacoes{
+    private $notificador;
+
+    public function __construct(INotificador $notificador)
+    {
+        $this->notificador = $notificador;
+    }
+
+    public function notificarUsuario($destinatario, $mensagem)
+    {
+        $this->notificador->enviar($destinario, $mensgem);
+    }
+}
+
+$sistemaEmail = new SistemaDeNotificacoes(new NotificadorEmail());
+$sistemaSMS = new SistemaDeNotificacoes(new NotificadorSMS());
+$sistemaWhatsapp = new SistemaDeNotificacoes(new Notificadorwhatsapp);
+
+$sistemaEmail->notificarUsurio("joao@email.com", "Seu Pedido foi confirmado");
+$sistemaSMS->notificarUsuario(17997651234, "Seu Pedido foi confirmado");
+$sistemaWhatsapp->notificarUsuario("17997651234", "Seu Pedido foi confirmado");
